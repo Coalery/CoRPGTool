af1a6c9ac80ea4d34925911182cf04e4c064ec8a
Coalery <doralife12@naver.com>
2020-02-17 23:09:58 +0900

    Package Add, MainUI.java class Add.

---
+++ src/org/coalery/MainUI.java
@@ -0,0 +1,17 @@
+package org.coalery;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class MainUI extends JFrame{
+    public MainUI() {
+        super("CoRPGTool");
+        addWindowListener(new WindowAdapter() {public void windowClosing(WindowEvent e) { System.exit(0); }});
+
+        setSize(Toolkit.getDefaultToolkit().getScreenSize());
+
+        setVisible(true);
+    }
+}
