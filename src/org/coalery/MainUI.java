c924606df2d5d90e8607ec7253e6b35980915912
Coalery <doralife12@naver.com>
2020-02-20 23:58:22 +0900

    SplitPane Divider Size to 1 > Still Fixing

--- src/org/coalery/MainUI.java
+++ src/org/coalery/MainUI.java
@@ -12,6 +12,7 @@ public class MainUI extends JFrame{
     public MainUI() {
         super("CoRPGTool");
         addWindowListener(new WindowAdapter() {public void windowClosing(WindowEvent e) { System.exit(0); }});
+        setDefaultLookAndFeelDecorated(true);
 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setSize((int)(screenSize.width * 0.75f), (int)(screenSize.height * 0.75f));
@@ -19,10 +20,9 @@ public class MainUI extends JFrame{
         MapTab mapTab = new MapTab();
         PropertyTab propertyTab = new PropertyTab();
 
-        JSplitPane map_property_SplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, mapTab, propertyTab);
+        ZeroSizeSplitPane map_property_SplitPane = new ZeroSizeSplitPane(JSplitPane.HORIZONTAL_SPLIT, mapTab, propertyTab);
         map_property_SplitPane.setOneTouchExpandable(true);
         map_property_SplitPane.setDividerLocation((int)(getWidth() * 0.75f));
-
 
         mapTab.setMinimumSize(new Dimension(150, 0));
         propertyTab.setMinimumSize(new Dimension(150, 0));
--- src/org/coalery/ZeroSizeSplitPane.java
+++ src/org/coalery/ZeroSizeSplitPane.java
@@ -1,6 +1,7 @@
 package org.coalery;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.plaf.basic.BasicSplitPaneDivider;
 import javax.swing.plaf.basic.BasicSplitPaneUI;
 import java.awt.*;
@@ -11,7 +12,8 @@ public class ZeroSizeSplitPane extends JSplitPane {
     private int dividerDragSize = 9;
     private int dividerDragOffset = 4;
 
-    public ZeroSizeSplitPane() {
+    public ZeroSizeSplitPane(int orientation, Component leftComponent, Component rightComponent) {
+        super(orientation, leftComponent, rightComponent);
         setDividerSize(1);
         setContinuousLayout(true);
     }
@@ -20,7 +22,56 @@ public class ZeroSizeSplitPane extends JSplitPane {
     public void doLayout() {
         super.doLayout();
 
+        System.out.println(getUI());
+
         BasicSplitPaneDivider divider = ((BasicSplitPaneUI)getUI()).getDivider();
         Rectangle bounds = divider.getBounds();
+
+        if(orientation == HORIZONTAL_SPLIT) {
+            bounds.x -= dividerDragOffset;
+            bounds.width = dividerDragSize;
+        } else {
+            bounds.y -= dividerDragOffset;
+            bounds.height = dividerDragSize;
+        }
+        divider.setBounds(bounds);
+    }
+
+    @Override
+    public void updateUI() {
+
+    }
+
+    private class ZeroSizeDividerUI extends BasicSplitPaneUI {
+        @Override
+        public BasicSplitPaneDivider createDefaultDivider() {
+            return new ZeroSizeDivider(this);
+        }
+    }
+
+    private class ZeroSizeDivider extends BasicSplitPaneDivider {
+        public ZeroSizeDivider(BasicSplitPaneUI ui) {
+            super(ui);
+            super.setBounds(null);
+            setBackground(UIManager.getColor("controlShadow"));
+        }
+
+        @Override
+        public void setBorder(Border border) {}
+
+        @Override
+        public void paint(Graphics g) {
+            g.setColor(getBackground());
+            if(orientation == HORIZONTAL_SPLIT)
+                g.drawLine(dividerDragOffset, 0, dividerDragOffset, getHeight() - 1);
+            else
+                g.drawLine(0, dividerDragOffset, getWidth() - 1, dividerDragOffset);
+        }
+
+        @Override
+        protected void dragDividerTo(int location) { super.dragDividerTo(location + dividerDragOffset); }
+
+        @Override
+        protected void finishDraggingTo(int location) { super.finishDraggingTo(location + dividerDragOffset); }
     }
 }
